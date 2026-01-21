ec98ea7ac569ce2216e67b7f5c048c31d565f1ff
MohZoro <mohamed.zoro4242@gmail.com>
2026-01-21 05:40:23 +0200

    added spawning pieces

--- Assets/Scripts/Manger.cs
+++ Assets/Scripts/Manger.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class Manger : MonoBehaviour
 {
-    Image img;
+    public GameObject Tile;
+    public GameObject Pawn;
+    public GameObject Rock;
+    public GameObject Bishop;
+    public GameObject Knight;
+    public GameObject Queen;
+    public GameObject King;
+
     int startX = -425;
     int startY = 435;
 
     void Start()
     {
-        img = transform.Find("Tile").GetComponent<Image>();
         Color32 a = new Color32(0xFF, 0xEF, 0xD3, 0xFF);
         Color32 b = new Color32(0x26, 0x24, 0x24, 0xFF);
 
@@ -21,7 +27,7 @@ public class Manger : MonoBehaviour
         {
             for (int j = 0; j < 8; j++)
             {
-                GameObject x = Instantiate(img.gameObject, transform);
+                GameObject x = Instantiate(Tile.gameObject, transform);
                 x.GetComponent<RectTransform>().localPosition = new Vector2(startX + j * 125, startY - i * 125);
 
                 x.GetComponent<Image>().color = f ? a : b;
@@ -29,13 +35,49 @@ public class Manger : MonoBehaviour
                 x.name = "Tile " + Convert.ToChar((j + 'a')) + (8 - i).ToString();
 
                 f = !f;
-
-                x.SetActive(true);
             }
 
             f = !f;
         }
+
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject p1 = Instantiate(Pawn, transform.Find("Tile " + Convert.ToChar(i + 'a') + '7'));
+
+            p1.GetComponent<RectTransform>().localPosition = Vector2.zero;
+
+            GameObject p2 = Instantiate(Pawn, transform.Find("Tile " + Convert.ToChar(i + 'a') + '2'));
+            p2.GetComponent<RectTransform>().localPosition = Vector2.zero;
+        }
+
+        GameObject r1 = Instantiate(Rock, transform.Find("Tile a8"));
+        GameObject r2 = Instantiate(Rock, transform.Find("Tile h8"));
+
+        GameObject r3 = Instantiate(Rock, transform.Find("Tile a1"));
+        GameObject r4 = Instantiate(Rock, transform.Find("Tile h1"));
+
+        GameObject k1 = Instantiate(Knight, transform.Find("Tile b8"));
+        GameObject k2 = Instantiate(Knight, transform.Find("Tile g8"));
+
+        GameObject k3 = Instantiate(Knight, transform.Find("Tile b1"));
+        GameObject k4 = Instantiate(Knight, transform.Find("Tile g1"));
+
+        GameObject b1 = Instantiate(Bishop, transform.Find("Tile c8"));
+        GameObject b2 = Instantiate(Bishop, transform.Find("Tile f8"));
+
+        GameObject b3 = Instantiate(Bishop, transform.Find("Tile c1"));
+        GameObject b4 = Instantiate(Bishop, transform.Find("Tile f1"));
+
+        GameObject q1 = Instantiate(Queen, transform.Find("Tile d8"));
+
+        GameObject q2 = Instantiate(Queen, transform.Find("Tile d1"));
+
+        GameObject ki1 = Instantiate(King, transform.Find("Tile e8"));
+        
+        GameObject ki2 = Instantiate(King, transform.Find("Tile e1"));
     }
+
+
 
 
     void Update()
