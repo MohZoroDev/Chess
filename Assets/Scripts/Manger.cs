a8178c06b8daafb74817d953ba97c7da8568b511
MohZoro <mohamed.zoro4242@gmail.com>
2026-01-21 05:05:26 +0200

    added chess board genration

---
+++ Assets/Scripts/Manger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Manger : MonoBehaviour
+{
+    Image img;
+    int startX = -425;
+    int startY = 435;
+
+    void Start()
+    {
+        img = transform.Find("Tile").GetComponent<Image>();
+        Color32 a = new Color32(0xFF, 0xEF, 0xD3, 0xFF);
+        Color32 b = new Color32(0x26, 0x24, 0x24, 0xFF);
+
+        bool f = true;
+
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                GameObject x = Instantiate(img.gameObject, transform);
+                x.GetComponent<RectTransform>().localPosition = new Vector2(startX + j * 125, startY - i * 125);
+
+                x.GetComponent<Image>().color = f ? a : b;
+
+                f = !f;
+
+                x.SetActive(true);
+            }
+
+            f = !f;
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+}
